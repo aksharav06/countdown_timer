dee434fe14e8decb8bcd0a093bb525eabf979156
aksharav06 <80166866+aksharav06@users.noreply.github.com>
2022-03-20 15:44:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const dayEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const myBirthday = '15 July 2022';
+
+function countdown() {
+    const myBirthdayDate = new Date(myBirthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (myBirthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    dayEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
